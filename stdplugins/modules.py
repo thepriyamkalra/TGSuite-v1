a2af50bdb78ba40655a45b61c0d735a92be895e8
authoritydmc <com.mailuser@gmail.com>
2020-02-15 12:27:15 +0530

    modules.py ..merge it

---
+++ stdplugins/modules.py
@@ -0,0 +1,25 @@
+# For UniBorg
+# By Priyam Kalra
+# modified by @authoritydmc
+# Syntax (.modules)
+from telethon import events
+from uniborg.util import admin_cmd
+import asyncio
+from telethon.tl import functions, types
+from sql_helpers.global_variables_sql import MODULE_LIST
+
+
+@borg.on(admin_cmd(pattern="modules ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    modules = "**List of available modules:**\n"
+    MODULE_LIST.sort()
+    prev="1"
+    for module in MODULE_LIST:
+        if prev[0]!=module[0]:
+            modules+=f"\n\n\t{module[0].upper()}\n\n"
+        modules += f"~ ```{module}```\n"
+        prev=module
+    modules += "\n\n**Tip --> Use .syntax <module_name> for more info.**"
+    await event.edit(modules)
