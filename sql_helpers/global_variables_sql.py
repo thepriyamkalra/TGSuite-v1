ddd9ab7a619a04c828869c38ade4106e4e601b60
Raj dubey <com.mailuser@gmail.com>
2020-02-15 11:39:23 +0530

    tuning

--- sql_helpers/global_variables_sql.py
+++ sql_helpers/global_variables_sql.py
@@ -16,8 +16,7 @@ class MODULE_LISTS(list):
     def __init__(self):
         self.MODULES=[]
     def append(self,item):
-        self.MODULES.append(item)
-        
+        self.MODULES.append(item)        
     def __str__(self):
         self.MODULES.sort()
         return str(self.MODULES)
@@ -25,15 +24,12 @@ class MODULE_LISTS(list):
         return len(self.MODULES)
     def __delitem__(self, index):
         self.MODULES.__delitem__(index - 1)
-
     def insert(self, index, value):
         self.MODULES.insert(index - 1, value)
-
     def __setitem__(self, index, value):
         self.MODULES.__setitem__(index - 1, value)
-
     def __getitem__(self, index):
         return self.MODULES.__getitem__(index - 1)
     def __iter__(self):
-        return (item for item in self.MODULES.__iter__() )
+        return iter(self.MODULES)
 MODULE_LIST=MODULE_LISTS()
