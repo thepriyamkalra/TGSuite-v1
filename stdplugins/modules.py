bd5bcb9d6969dcd3e39831b686ca4540faac9be1
Raj dubey <com.mailuser@gmail.com>
2020-02-15 12:35:16 +0530

    modules.py sorted and listed with Alphabets

--- stdplugins/modules.py
+++ stdplugins/modules.py
@@ -13,12 +13,12 @@ from sql_helpers.global_variables_sql import MODULE_LIST
 async def _(event):
     if event.fwd_from:
         return
-    modules = "**List of available modules:**\n"
+    modules = "**List of available modules({0}):**\n".format(len(MODULE_LIST))
     MODULE_LIST.sort()
     prev="1"
     for module in MODULE_LIST:
         if prev[0]!=module[0]:
-            modules+=f"\n\n\t{module[0].upper()}\n\n"
+            modules+=f"\n\n\t{module[0].upper()}\t\n\n"
         modules += f"~ ```{module}```\n"
         prev=module
     modules += "\n\n**Tip --> Use .syntax <module_name> for more info.**"
