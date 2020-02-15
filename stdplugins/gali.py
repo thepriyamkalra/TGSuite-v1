a4c908ce84d032351ea412b2504b9aa387df7c8d
Raj dubey <com.mailuser@gmail.com>
2020-02-15 21:07:50 +0530

    fixed gali module

--- stdplugins/gali.py
+++ stdplugins/gali.py
@@ -18,9 +18,8 @@ async def _(event):
     if event.fwd_from:
         return
     args = event.pattern_match.group(1)
-    animal = ["मच्छर ", "\
+    animal = ["मच्छर ","\
 हाथी","गिरगिट", "गैंडा","खरगोश","बंदर ","लंगूर","सॉढ","सियार","बतख","गोरिल्ला"," नेवला","खटमल","घोंघा","छछून्‍दर"]
-    man_k==
     parts=["लुंड ","झाट","चुत","गांड ","गांड का कीड़ा","औलाद","दामाद","भाई"]
     man_slang=["बहनचोद","मादरचोद","बहिन के लौड़े","बहिन के टक्के","बेटीचोद ","भैंस की औलाद "]
     starts = ["क्या रे", "तुम", "अबे सुन चूतिये ", "साले ",
@@ -39,9 +38,10 @@ async def _(event):
     for insulting in range(args):
         start = random.choice(starts)
         parts_ch = random.choice(parts)
+        animal_ch=random.choice(animal)
         man_slang_ch = random.choice(man_slang)
         end = random.choice(ends)
-        insult = start+" के "+parts_ch+" "+man_slang_ch+" "+end
+        insult = start+" "+animal_ch+" के "+parts_ch+" "+man_slang_ch+" "+end
         insults+="\n"+insult
         log_insults += f"```{insult}```\n\n"
         reply_msg = await event.get_reply_message()
