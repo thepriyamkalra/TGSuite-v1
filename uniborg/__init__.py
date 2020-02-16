44c899e79653c904d6e51e7cdc708ea678741d6a
Raj dubey <com.mailuser@gmail.com>
2020-02-17 00:01:51 +0530

    added pip upgrade

--- uniborg/__init__.py
+++ uniborg/__init__.py
@@ -3,3 +3,13 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from .uniborg import *
+import os
+import re
+z=os.popen("pip  --version")
+sr=z.read()
+x = re.search(r"(\d+)\.(\d+)\.(\d+)", sr)
+initpipversion=x.group(1)
+if int(initpipversion) < 20:
+	os.system("pip install  --upgrade pip")
+else:
+	print("already upgraded version")
