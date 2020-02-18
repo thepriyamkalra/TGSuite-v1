b152de316d3245633e5710c90c444b67281f5fd4
Raj dubey <com.mailuser@gmail.com>
2020-02-19 00:04:00 +0530

    new mod

--- stdplugins/_inlinebot.py
+++ stdplugins/_inlinebot.py
@@ -7,7 +7,7 @@ from telethon import events, errors, custom
 from sql_helpers.global_variables_sql import MODULE_LIST
 import io
 
-if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
+if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
     async def inline_handler(event):
         builder = event.builder
