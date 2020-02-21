36c57d6530cf9a5c20fdd20d10f06aac55aa312b
Priyam Kalra <43999863+Priyam005@users.noreply.github.com>
2020-02-22 00:35:49 +0530

    add leech2drive, leech2tg & change dafault admin tag to admeme

--- stdplugins/bash.py
+++ stdplugins/bash.py
@@ -39,7 +39,7 @@ async def _(event):
     else:
         _o = o.split("\n")
         o = "`\n".join(_o)
-    OUTPUT = f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**stderr:** \n`{e}`\n**Output:**\n{o}"
+    OUTPUT = f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**stderr:** \n`{e}`\n**Output:**\n`{o}`"
     if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
             out_file.name = "exec.text"
