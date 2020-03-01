8c2b7c25629d848d00bb5243e320c6109a8e5dfb
Raj dubey <com.mailuser@gmail.com>
2020-03-01 11:11:06 +0530

    fix alive crash:

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -11,12 +11,16 @@ MODULE_LIST.append("alive")
 
 @borg.on(admin_cmd(pattern="alive ?(.*)", allow_sudo=True))  # pylint:disable=E0602
 async def _(event):
-    if event.fwd_from:
-        return
-    if Config.USER is not None:
-        user = f"\n```User: {Config.USER}```"
-    else:
-        user = " "
+	user=" "
+	try:
+	    if event.fwd_from:
+	        return
+	    if Config.USER is not None:
+	        user = f"\n```User: {Config.USER}```"
+	    else:
+	        user = " "
+	except :
+			user=" "
     help_string = f"BEASTBOT-REBORN v 1.2 is running.\n```Python {sys.version}```\n```Telethon {__version__}```\n```Build: {BUILD}```{str(user)}\n```By: @beast0110```\nDeploy Code [@Github](https://github.com/authoritydmc/BEASTBOT-REBORN)"
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER  # pylint:disable=E0602
     if tgbotusername is not None:
