4b19ba0a4853576f4597c1263c00434b554e54a9
Raj dubey <com.mailuser@gmail.com>
2020-04-02 15:58:32 +0530

    fix link pmpermit

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -1,12 +1,12 @@
 # For UniBorg
 # Syntax (.approve, .block)
-
+import sys
 import asyncio
 import json
 from telethon import events
 from telethon.tl import functions, types
 from sql_helpers.pmpermit_sql import is_approved, approve, disapprove, get_all_approved
-from sql_helpers.global_variables_sql import SYNTAX, MODULE_LIST,SUDO_USERS
+from sql_helpers.global_variables_sql import S  YNTAX, MODULE_LIST,SUDO_USERS
 from uniborg.util import admin_cmd
 
 ig_link="set IG_LINK in Heroku config"
@@ -34,7 +34,7 @@ UNIBORG_USER_BOT_WARN_ZERO = "```Blocked! Thanks for the spam.```"
 UNIBORG_USER_BOT_NO_WARN = """```
 Hee HAA! This is a bot. Don't fret.\nMy master hasn't approved you to PM.\nPlease
 wait for my master to look in, he mostly approves PMs.\n
-```"""+f"\nFacebook : test {fb_link} {Config.FB_LINK} [click here]({fb_link})\n\nInstagram: [Go here]({ig_link})\n\nGithub: [branch here]({github_link})\n"
+```"""+f"\nFacebook : test {fb_link} {ig_link} {github_link} [click here]({fb_link})\n\nInstagram: [Go here]({ig_link})\n\nGithub: [branch here]({github_link})\n"
 
 
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
