6da781e4a70a3abe3e906507eebd7e4b606fcf15
Raj dubey <com.mailuser@gmail.com>
2020-04-05 19:47:33 +0530

    get_size not found err fixed

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -50,6 +50,12 @@ async def _(event):
         await event.reply(help_string + "\n\n")
         await event.delete()
 
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
 
 SYNTAX.update({
     "alive": "\
