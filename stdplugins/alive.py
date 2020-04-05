11fc72db64e27be2baff009cdedd6dc6b4d02ee1
Raj dubey <com.mailuser@gmail.com>
2020-04-05 18:52:17 +0530

    minor changes

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -12,6 +12,13 @@ MODULE_LIST.append("alive")
 
 # if Config.BOT_USER is not None:
 #     username=Config.BOT_USER
+user_first_name=""
+user_last_name=""
+try:
+    userobj=  await borg.get_me()
+    user_last_name=userobj.last_name
+except:
+    user_last_name="error getting lastName @getme"
 
 @borg.on(admin_cmd(pattern="alive ?(.*)", allow_sudo=True))  # pylint:disable=E0602
 async def _(event):
@@ -25,11 +32,12 @@ async def _(event):
                     re.chat_id
                 )
             )
-        username=obj_user.first_name
+        user_first_name=obj_user.first_name
     except :
-        pass
+        user_first_name="Error at getting User Firstname"
+
  
-    help_string = f"BEASTBOT-REBORN v 1.3 is running for **{username}**.\n```Python {sys.version}```\n```Telethon {__version__}```\n```Build: {BUILD}```\nBy: @beast0110\nDeploy Code [@Github](https://github.com/authoritydmc/BEASTBOT-REBORN)"
+    help_string = f"BEASTBOT-REBORN v 1.3 is running for **{user_first_name}** \t{user_last_name}.\n```Python {sys.version}```\n```Telethon {__version__}```\n```Build: {BUILD}```\nBy: @beast0110\nDeploy Code [@Github](https://github.com/authoritydmc/BEASTBOT-REBORN)"
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER  # pylint:disable=E0602
     if tgbotusername is not None:
         results = await borg.inline_query(  # pylint:disable=E0602
