78982a1525e9c61d8fa43aaae175a0d327d4bf9c
Raj dubey <com.mailuser@gmail.com>
2020-04-05 17:33:45 +0530

    show firstName of user running the bot

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -9,14 +9,19 @@ from sql_helpers.global_variables_sql import SYNTAX, MODULE_LIST, BUILD
 MODULE_LIST.append("alive")
 username="set USER_NAME in Heroku Config"
 
-if Config.BOT_USER is not None:
-    username=Config.BOT_USER
+# if Config.BOT_USER is not None:
+#     username=Config.BOT_USER
 
 @borg.on(admin_cmd(pattern="alive ?(.*)", allow_sudo=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
-    help_string = f"BEASTBOT-REBORN v 1.3 is running for {username}.\n```Python {sys.version}```\n```Telethon {__version__}```\n```Build: {BUILD}```\nBy: @beast0110\nDeploy Code [@Github](https://github.com/authoritydmc/BEASTBOT-REBORN)"
+    try:
+        user_self = await  borg.getme()
+        username=user_self.first_name
+    except :
+        username="Failed to get userName from getme()"
+    help_string = f"BEASTBOT-REBORN v 1.3 is running for **{username}**.\n```Python {sys.version}```\n```Telethon {__version__}```\n```Build: {BUILD}```\nBy: @beast0110\nDeploy Code [@Github](https://github.com/authoritydmc/BEASTBOT-REBORN)"
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER  # pylint:disable=E0602
     if tgbotusername is not None:
         results = await borg.inline_query(  # pylint:disable=E0602
