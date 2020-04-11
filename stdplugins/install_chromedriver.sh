f4643c0c1e1f3cd8488f507438e9cdbc47cf5178
Raj dubey <com.mailuser@gmail.com>
2020-04-12 01:32:08 +0530

    fix install

--- stdplugins/install_chromedriver.sh
+++ stdplugins/install_chromedriver.sh
@@ -7,14 +7,6 @@
 
 # cp chromedriver /usr/local/bin
 
-BUILD_DIR=${1:-}
-CACHE_DIR=${2:-}
-ENV_DIR=${3:-}
-
-BIN_DIR=$BUILD_DIR/.chromedriver/bin
-mkdir -p $BIN_DIR
-
 
 wget  https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_linux64.zip
-unzip -o chromedriver_linux64.zip -d $BIN_DIR
-unzip chromedriver_linux64.zip 
+unzip -o chromedriver_linux64.zip -d /app/.chromedriver/bin/
