7c20d6fac2c3aeece7bd2481e9c6476c71ebba9a
authoritydmc <com.mailuser@gmail.com>
2020-04-12 02:00:49 +0530

    Test (#5)
    * remove unwanted var file
    
    * fixed private chat issue as well as some other issues
    
    * ignore pyc files
    
    * module add
    
    * minor changes
    
    * ignore load plugin file
    
    * rm load plugin file
    
    * fix crashing due to fileNotfound
    
    * minor fixes
    
    * minor fix
    
    * fix
    
    * ignore DOWNLOADS
    
    * remove not working module from list
    
    * ignore test module
    
    * google image search working now
    
    * goggle image working
    
    * update #google image module
    
    * fix bug img directory not deleting
    
    * update install.sh
    
    * fix for driver error
    
    * install fix

--- stdplugins/install_chromedriver.sh
+++ stdplugins/install_chromedriver.sh
@@ -1,12 +1,3 @@
-
 #!/bin/bash
-
-# wget https://chromedriver.storage.googleapis.com/81.0.4044.69/chromedriver_linux64.zip
-
-# unzip chromedriver_linux64.zip
-
-# cp chromedriver /usr/local/bin
-
-
 wget  https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_linux64.zip
 unzip -o chromedriver_linux64.zip -d /app/.chromedriver/bin/
