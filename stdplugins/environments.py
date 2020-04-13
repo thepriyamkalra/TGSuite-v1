57e6c57b6ddc3acc54c1e243502da6612a6a0813
Raj dubey <com.mailuser@gmail.com>
2020-04-13 22:50:11 +0530

    fixed env module

--- stdplugins/environments.py
+++ stdplugins/environments.py
@@ -9,17 +9,16 @@ async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    # await event.edit("test "+input_str)
-    if input_str :
-        reslt=f"Total env variable are **{len(os.environ.keys)}**\n"
-        for k in os.environ.keys():
-            reslt+=f"```{k}```\n"
+    if not input_str :
+        reslt=f"Total env variable are **{len(os.environ.keys())}**\n"
+        for k in sorted(os.environ.keys()):
+            reslt+=f"```{k}``` \n"
         await  event.edit(reslt)
 
     else:
         try:
             reslt=os.environ.get(input_str,"no such environment variable exist")
-            await  event.edit(f"```{input_str}``` -> {reslt}")
+            await  event.edit(f"```{input_str}``` -> ```{reslt}```")
         except :
             await event.edit("error getting env variable")
     
@@ -29,14 +28,16 @@ async def _(event):
     if event.fwd_from:
         return
     variable_name = event.pattern_match.group(1)
-    if not variable_name:
+    if variable_name:
         abe=await event.get_reply_message()
-        env_value=abe.text
-        if env_value is None:
-            await event.edit("Reply to a environment value you want to set first")
-        else:
+        try:
+            env_value=abe.text
             os.environ[variable_name]=env_value
-            print(f"set the env value {variable_name} to {env_value}")
+            logger.info(f"set the env value {variable_name} to {env_value}")
+            await  event.edit(f"set the value ```{env_value}``` for ```{variable_name}``` is **successfull**")
+        except :
+            await event.edit(f"Reply to a environment value you want to set first for ```{variable_name}```")
+
     else:
         await  event.edit(f"```Give environment variable name first for which you want to set.```")
 
