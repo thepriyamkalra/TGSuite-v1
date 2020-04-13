df53e49adde3ffe74c0fb762cd8053f9eda3af71
Raj dubey <com.mailuser@gmail.com>
2020-04-13 21:49:54 +0530

    environments is ready not tested yet

--- stdplugins/environments.py
+++ stdplugins/environments.py
@@ -1,16 +1,52 @@
 from uniborg.util import admin_cmd
 from sql_helpers.global_variables_sql import  SYNTAX, MODULE_LIST
 import asyncio
+from selenium import webdriver
 import os
 import time
-MODULE_LIST.append("test (image search query)")
+import io
+import requests
+from PIL import Image
+import hashlib
+MODULE_LIST.append("getenv")
 
-def progress(current, total):
-    logger.info("Downloaded {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
-@borg.on(admin_cmd(pattern="img (.*)"))
+@borg.on(admin_cmd(pattern="getenv ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    await event.edit("test "+input_str)
+    # await event.edit("test "+input_str)
+    if input_str is None:
+        reslt=f"Total env variable are **{len(os.environ.keys)}**\n"
+        for k in os.environ.keys():
+            reslt+=f"```{k}```\n"
+        await  event.edit(reslt)
+
+    else:
+        try:
+            reslt=os.environ.get(input_str,"no such environment variable exist")
+            await  event.edit(f"```{input_str}``` -> {reslt}")
+        except :
+            await event.edit("error getting env variable")
     
+
+@borg.on(admin_cmd(pattern="setenv ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    variable_name = event.pattern_match.group(1)
+    # await event.edit("test "+input_str)
+    if variable_name not None:
+        abe=await event.get_reply_message()
+        env_value=abe.text
+        if env_value is None:
+            await event.edit("Reply to a environment value you want to set first")
+        else:
+            os.environ[variable_name]=env_value
+            print(f"set the env value {variable_name} to {env_value}")
+
+
+    else:
+        await  event.edit(f"```Give environment variable name first for which you want to set.```")
+
+
