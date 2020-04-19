8fbf4b0f7ef07bda10c22fd1f55a0b333336bb30
Priyam Kalra <43999863+Priyam005@users.noreply.github.com>
2020-04-19 13:04:49 +0500

    removed redundant code - pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -6,58 +6,57 @@ import json
 from telethon import events
 from telethon.tl import functions, types
 from sql_helpers.pmpermit_sql import is_approved, approve, disapprove, get_all_approved
-from sql_helpers.global_variables_sql import SYNTAX, MODULE_LIST,SUDO_USERS
+from sql_helpers.global_variables_sql import SYNTAX, MODULE_LIST, SUDO_USERS
 from uniborg.util import admin_cmd
 
 
 MODULE_LIST.append("pmpermit")
+
 borg.storage.PM_WARNS = {}
-st="H9I_"
 borg.storage.PREV_REPLY_MESSAGE = {}
-z=int(''.join([str(ord(x)) for x in st]))
-CONTINOUS_MSG_COUNT=0 #for bot and spam protection
- #for bot verification
+
+
+BAALAJI_TG_USER_BOT = "```My Master hasn't approved you to PM.```"
+TG_COMPANION_USER_BOT = "```Wait for my masters response.\nDo not spam his pm if you do not want to get blocked.```"
 UNIBORG_USER_BOT_WARN_ZERO = "```Blocked! Thanks for the spam.```"
 UNIBORG_USER_BOT_NO_WARN = """```
-Hee HAA! This is a bot. Don't fret.\nMy master hasn't approved you to PM.\nPlease
-wait for my master to look in, he mostly approves PMs.\n
-
-```\n
-
-"""
+Bleep blop! This is a bot. Don't fret.\nMy master hasn't approved you to PM.\nPlease wait for my master to look in, he mostly approves PMs.\nAs far as I know, he doesn't usually approve retards though.
+\nIf you continue sending messages you will be blocked.```"""
 
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def monito_p_m_s(event):
     sender = await event.get_sender()
     current_message_text = event.message.message.lower()
-    PREVIOUS_MSG=current_message_text
+    if current_message_text == BAALAJI_TG_USER_BOT or \
+        current_message_text == TG_COMPANION_USER_BOT or \
+        current_message_text == UNIBORG_USER_BOT_NO_WARN:
+        # userbot's should not reply to other userbot's
+        # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
+        return False
     if Config.NO_P_M_SPAM and not sender.bot:
         chat = await event.get_chat()
         if not is_approved(chat.id) and chat.id != borg.uid:
             logger.info(chat.stringify())
             logger.info(borg.storage.PM_WARNS)
-            if chat.id in SUDO_USERS:
-                await event.reply("Wait, that looks my creator!")
-                await event.reply("Approving..")
-                approve(chat.id,"SUDO USER")
-                return
+            if chat.id in SUDO_USERS;
+                await event.edit("Oh wait, that looks like my master!")
+                await event.edit("Approving..")
+                approve(chat.id, "SUDO_USER")
             if chat.id not in borg.storage.PM_WARNS:
                 borg.storage.PM_WARNS.update({chat.id: 0})
             if borg.storage.PM_WARNS[chat.id] == Config.MAX_FLOOD_IN_P_M_s:
                 r = await event.reply(UNIBORG_USER_BOT_WARN_ZERO)
                 await asyncio.sleep(3)
-                
                 await borg(functions.contacts.BlockRequest(chat.id))
                 if chat.id in borg.storage.PREV_REPLY_MESSAGE:
                     await borg.storage.PREV_REPLY_MESSAGE[chat.id].delete()
                 borg.storage.PREV_REPLY_MESSAGE[chat.id] = r
                 return
-            if borg.storage.PM_WARNS[chat.id] < 2:
-                r = await event.reply(UNIBORG_USER_BOT_NO_WARN)
-                if chat.id in borg.storage.PREV_REPLY_MESSAGE:
-                    await borg.storage.PREV_REPLY_MESSAGE[chat.id].delete()
-                borg.storage.PREV_REPLY_MESSAGE[chat.id] = r
+            r = await event.reply(UNIBORG_USER_BOT_NO_WARN)
             borg.storage.PM_WARNS[chat.id] += 1
+            if chat.id in borg.storage.PREV_REPLY_MESSAGE:
+                await borg.storage.PREV_REPLY_MESSAGE[chat.id].delete()
+            borg.storage.PREV_REPLY_MESSAGE[chat.id] = r
 
 
 @borg.on(admin_cmd("approve ?(.*)"))
@@ -121,8 +120,7 @@ async def approve_p_m(event):
     else:
         await event.edit(APPROVED_PMs)
 
-if z not in SUDO_USERS:
-    SUDO_USERS.append(z)
+
 
 SYNTAX.update({
     "pmpermit": "\
