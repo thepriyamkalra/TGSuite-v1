3fa83ab4cfeb959c02f161f29dc5c8448038db48
TechyNewbie <63805243+Techy05@users.noreply.github.com>
2020-06-25 22:31:09 +0530

    Update android.py

--- plugins/android.py
+++ plugins/android.py
@@ -5,23 +5,18 @@
 #
 """ Userbot module containing commands related to android"""
 
-from sql.global_variables_sql import SYNTAX, MODULE_LIST
+from userbot import syntax
 from telethon import events
 import re
 from requests import get
 from bs4 import BeautifulSoup
-
-from userbot import CMD_HELP
-from userbot.utils import register
-
-MODULE_LIST.append("android")
 
 GITHUB = 'https://github.com'
 DEVICES_DATA = 'https://raw.githubusercontent.com/androidtrackers/' \
                'certified-android-devices/master/devices.json'
 
 
-@borg.on(events.NewMessage(pattern=r"\.magisk", outgoing=True))
+@bot.on(command("magisk ?(.*)"))
 async def magisk(event):
     if event.fwd_from:
         return
@@ -40,123 +35,6 @@ async def magisk(event):
                     f'[Uninstaller]({data["uninstaller"]["link"]})\n'
     await event.edit(releases)
 
-@register(outgoing=True, pattern=r"^.device(?: |$)(\S*)")
-async def device_info(request):
-    """ get android device basic info from its codename """
-    textx = await request.get_reply_message()
-    device = request.pattern_match.group(1)
-    if device:
-        pass
-    elif textx:
-        device = textx.text
-    else:
-        await request.edit("`Usage: .device <codename> / <model>`")
-        return
-    found = [
-        i for i in get(DEVICES_DATA).json()
-        if i["device"] == device or i["model"] == device
-    ]
-    if found:
-        reply = f'Search results for {device}:\n\n'
-        for item in found:
-            brand = item['brand']
-            name = item['name']
-            codename = item['device']
-            model = item['model']
-            reply += f'{brand} {name}\n' \
-                f'**Codename**: `{codename}`\n' \
-                f'**Model**: {model}\n\n'
-    else:
-        reply = f"`Couldn't find info about {device}!`\n"
-    await request.edit(reply)
-
-
-@register(outgoing=True, pattern=r"^.codename(?: |)([\S]*)(?: |)([\s\S]*)")
-async def codename_info(request):
-    """ search for android codename """
-    textx = await request.get_reply_message()
-    brand = request.pattern_match.group(1).lower()
-    device = request.pattern_match.group(2).lower()
-    if brand and device:
-        pass
-    elif textx:
-        brand = textx.text.split(' ')[0]
-        device = ' '.join(textx.text.split(' ')[1:])
-    else:
-        await request.edit("`Usage: .codename <brand> <device>`")
-        return
-    found = [
-        i for i in get(DEVICES_DATA).json()
-        if i["brand"].lower() == brand and device in i["name"].lower()
-    ]
-    if len(found) > 8:
-        found = found[:8]
-    if found:
-        reply = f'Search results for {brand.capitalize()} {device.capitalize()}:\n\n'
-        for item in found:
-            brand = item['brand']
-            name = item['name']
-            codename = item['device']
-            model = item['model']
-            reply += f'{brand} {name}\n' \
-                f'**Codename**: `{codename}`\n' \
-                f'**Model**: {model}\n\n'
-    else:
-        reply = f"`Couldn't find {device} codename!`\n"
-    await request.edit(reply)
-
-
-@register(outgoing=True, pattern=r"^.specs(?: |)([\S]*)(?: |)([\s\S]*)")
-async def devices_specifications(request):
-    """ Mobile devices specifications """
-    textx = await request.get_reply_message()
-    brand = request.pattern_match.group(1).lower()
-    device = request.pattern_match.group(2).lower()
-    if brand and device:
-        pass
-    elif textx:
-        brand = textx.text.split(' ')[0]
-        device = ' '.join(textx.text.split(' ')[1:])
-    else:
-        await request.edit("`Usage: .specs <brand> <device>`")
-        return
-    all_brands = BeautifulSoup(
-        get('https://www.devicespecifications.com/en/brand-more').content,
-        'lxml').find('div', {
-            'class': 'brand-listing-container-news'
-        }).findAll('a')
-    brand_page_url = None
-    try:
-        brand_page_url = [
-            i['href'] for i in all_brands if brand == i.text.strip().lower()
-        ][0]
-    except IndexError:
-        await request.edit(f'`{brand} is unknown brand!`')
-    devices = BeautifulSoup(get(brand_page_url).content, 'lxml') \
-        .findAll('div', {'class': 'model-listing-container-80'})
-    device_page_url = None
-    try:
-        device_page_url = [
-            i.a['href']
-            for i in BeautifulSoup(str(devices), 'lxml').findAll('h3')
-            if device in i.text.strip().lower()
-        ]
-    except IndexError:
-        await request.edit(f"`can't find {device}!`")
-    if len(device_page_url) > 2:
-        device_page_url = device_page_url[:2]
-    reply = ''
-    for url in device_page_url:
-        info = BeautifulSoup(get(url).content, 'lxml')
-        reply = '\n**' + info.title.text.split('-')[0].strip() + '**\n\n'
-        info = info.find('div', {'id': 'model-brief-specifications'})
-        specifications = re.findall(r'<b>.*?<br/>', str(info))
-        for item in specifications:
-            title = re.findall(r'<b>(.*?)</b>', item)[0].strip()
-            data = re.findall(r'</b>: (.*?)<br/>', item)[0]\
-                .replace('<b>', '').replace('</b>', '').strip()
-            reply += f'**{title}**: {data}\n'
-    await request.edit(reply)
 
 
 @borg.on(events.NewMessage(pattern=r"\.twrp", outgoing=True))
@@ -190,17 +68,11 @@ async def twrp(event):
     await event.edit(reply)
 
 
-SYNTAX.update({
+syntax.update({
     "android": "\
 **Requested module --> Android**\
 \n\n```.magisk```\
 \nUsage: Get latest Magisk releases\
-\n\n```.device <codename>```\
-\nUsage: Get info about android device codename or model.\
-\n\n```.codename <brand> <device>```\
-\nUsage: Search for android device codename.\
-\n\n```.specs <brand> <device>```\
-\nUsage: Get device specifications info.\
 \n\n```.twrp <codename>```\
 \nUsage: Get latest twrp download for android device.\
 "
