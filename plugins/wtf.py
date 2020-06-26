9048a605e0ef38d76cd7fe81a66d7bb5bac77641
TechyNewbie <63805243+Techy05@users.noreply.github.com>
2020-06-26 11:34:43 +0530

    Update wtf.py

--- plugins/wtf.py
+++ plugins/wtf.py
@@ -1,38 +1,29 @@
-#By X-tra-Telegram Modz Geng
+#By @XtraModZ geng
 """Emoji
 
 Available Commands:
-
 .WTF
-
 .gei"""
-
-from telethon import events
 
 import asyncio
 
-from userbot.utils import admin_cmd
-
-@borg.on(admin_cmd("(.*)"))
-async def _(event):
-    if event.fwd_from:
+@bot.on(command("WTF ?(.*)"))
+async def _(tf):
+    if tf.fwd_from:
         return
     animation_interval = 0.2
     animation_ttl = range(0, 4)
-    input_str = event.pattern_match.group(1)
-    if input_str == "WTF":
-        await event.edit(input_str)
-        animation_chars = [
-            "**What**",
-            "**What The**",
-            "**What The F**",
-            "**What The F**, __Dude__?"
-        ]
+    animation_chars = [
+        "**What**",
+        "**What The**",
+        "**What The F**",
+        "**What The F**, __Brah__?"
+    ]
 
-        for i in animation_ttl:
+    for i in animation_ttl:
         	
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 4])
+        await asyncio.sleep(animation_interval)
+        await tf.edit(animation_chars[i % 4])
 
 
 
@@ -40,29 +31,26 @@ async def _(event):
 #Sorry Priyam, Don't take it seriously XD
 #Syntax .gei"""
 
-from telethon import events
-
 import asyncio
 
-from userbot.utils import admin_cmd
-
-@borg.on(admin_cmd("(.*)"))
+@bot.on(command("gei ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
+    thing = event.pattern_match.group(1)
+    if not thing:
+        ton = await event.get_reply_message()
+        thing = ton.text
     animation_interval = 0.2
     animation_ttl = range(0, 4)
-    input_str = event.pattern_match.group(1)
-    if input_str == "gei":
-        await event.edit(input_str)
-        animation_chars = [
-            "**“It**",
-            "**“It Is**",
-            "**“It Is Gei”**",
-            "**“It Is Gei”** __- Priyam Kalra__"
-        ]
+    animation_chars = [
+        f"**“{thing}**",
+        f"**“{thing} Is**",
+        f"**“{thing} Is Gei”**",
+        f"**“{thing} Is Gei”** __- Priyam Kalra__"
+    ]
 
-        for i in animation_ttl:
+    for i in animation_ttl:
         	
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 4])
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
