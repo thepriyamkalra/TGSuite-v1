ee236fde6a15ed159fa1d26cb9f838d642fbd0bd
your_username <43999863+Priyam005@users.noreply.github.com>
2020-07-07 23:47:34 +0530

    Add unlocks module

---
+++ plugins/unlocks.py
@@ -0,0 +1,42 @@
+# For The-TG-Bot-3.0
+# By Priyam Kalra
+
+import os
+from userbot import syntax
+
+
+@bot.on(command())  # outgoing=True by default
+async def watch(event):
+    if event.chat_id not in Config.UNLOCKED_CHATS:
+        return
+    if not os.path.isdir(Config.DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.DOWNLOAD_DIRECTORY)
+    if event.sticker:
+        reply = await event.get_reply_message()
+        file = event.sticker
+        file_name = "sticker.png"
+        to_download_directory = Config.DOWNLOAD_DIRECTORY
+        downloaded_file_name = os.path.join(to_download_directory, file_name)
+        downloaded_file_name = await bot.download_media(
+            file,
+            downloaded_file_name
+        )
+        if os.path.exists(downloaded_file_name):
+            await bot.send_file(
+                event.chat_id,
+                downloaded_file_name,
+                force_document=False,
+                supports_streaming=False,
+                allow_cache=False,
+                reply_to=reply,
+            )
+            os.remove(downloaded_file_name)
+
+syntax.update({
+    "unlocks": "\
+Usage: Worksaround userbot/tg bot \"admin-only\" locks in chats.\
+\nWhat can be sent in chats where its locked (for now):\
+- stickers (only if sending pictures is allowed)\
+\n\nYou need to add a chat to `UNLOCKED_CHATS` enviroment variables for this to take effect in those chats.\
+"
+})
--- production.py
+++ production.py
@@ -32,3 +32,4 @@ class Config(object):
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     TG_APP_NAME = os.environ.get("TG_APP_NAME", None)
     STICKER_PACK = os.environ.get("STICKER_PACK", None)
+    UNLOCKED_CHATS = os.environ.get("UNLOCKED_CHATS", None)
