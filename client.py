d3304fb7522c6ad04398cb82665581fdb5e928a7
Priyam Kalra <43999863+justaprudev@users.noreply.github.com>
2020-09-05 14:30:33 +0530

    Grab the git repository url from environment variables

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import subprocess
+import os
+GITHUB_REPO_LINK = os.environ.get("GITHUB_REPO_LINK", "https://github.com/justaprudev/The-TG-Bot")
 print("Downloading latest version of The-TG-Bot..")
-subprocess.run("git clone https://github.com/justaprudev/The-TG-Bot git && mv git/* . && rm -rf git", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+subprocess.run(f"git clone {GITHUB_REPO_LINK} git && mv git/* . && rm -rf git", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
 print("Starting process with command `python3 -m thetgbot`")
 print("State changed from starting to up")
 subprocess.run("python3 -m thetgbot", shell=True)
