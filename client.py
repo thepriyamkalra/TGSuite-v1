81dec1bb04001106646cfafea5e76da29fa41eb6
Priyam Kalra <43999863+justaprudev@users.noreply.github.com>
2020-09-16 10:31:30 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
+import os
 from git import Repo
 from shutil import copytree, rmtree
-from os.environ import get
-GITHUB_REPO_LINK = get("GITHUB_REPO_LINK", "https://github.com/justaprudev/The-TG-Bot")
+GITHUB_REPO_LINK = os.environ.get("GITHUB_REPO_LINK", "https://github.com/justaprudev/The-TG-Bot")
 print("Downloading latest version of The-TG-Bot..")
 Repo.clone_from(GITHUB_REPO_LINK, "package")
 copytree("package", ".", dirs_exist_ok=True)
