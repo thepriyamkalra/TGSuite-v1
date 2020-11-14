9f6bf12695a641640863851c0bae0f65e7c4edc9
Priyam Kalra <43999863+justaprudev@users.noreply.github.com>
2020-11-14 19:01:39 +0530

    Add testing support (maybe?)

--- client.py
+++ client.py
@@ -1,9 +1,14 @@
 import os
 from git import Repo
 from shutil import copytree, rmtree
-GITHUB_REPO_LINK = os.environ.get("GITHUB_REPO_LINK", "https://github.com/justaprudev/The-TG-Bot")
+GITHUB_REPO = os.environ.get("GITHUB_REPO_LINK", "https://github.com/justaprudev/The-TG-Bot").split()
+GITHUB_REPO_LINK = GITHUB_REPO[0]
+try:
+  GITHUB_REPO_BRANCH = GITHUB_REPO[1]
+except:
+  GITHUB_REPO_BRANCH = "v3"
 print("Downloading latest version of The-TG-Bot..")
-Repo.clone_from(GITHUB_REPO_LINK, "package")
-copytree("package", ".", dirs_exist_ok=True)
-rmtree("package")
+Repo.clone_from(GITHUB_REPO_LINK, ".ota", branch=GITHUB_REPO_BRANCH)
+copytree(".ota", ".", dirs_exist_ok=True)
+rmtree(".ota")
 import thetgbot
