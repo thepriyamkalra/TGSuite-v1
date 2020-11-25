62375553ccf83961cc128e3d70567be8cd834a4b
Priyam Kalra <43999863+justaprudev@users.noreply.github.com>
2020-11-25 18:19:14 +0530

    Added testing support and now uses GitPython (#42)

--- client.py
+++ client.py
@@ -1,8 +1,14 @@
-import subprocess
 import os
-GITHUB_REPO_LINK = os.environ.get("GITHUB_REPO_LINK", "https://github.com/justaprudev/The-TG-Bot")
+from git import Repo
+from shutil import copytree, rmtree
+GITHUB_REPO = os.environ.get("GITHUB_REPO_LINK", "https://github.com/justaprudev/The-TG-Bot").split()
+GITHUB_REPO_LINK = GITHUB_REPO[0]
+try:
+  GITHUB_REPO_BRANCH = GITHUB_REPO[1]
+except:
+  GITHUB_REPO_BRANCH = "v3"
 print("Downloading latest version of The-TG-Bot..")
-subprocess.run(f"git clone {GITHUB_REPO_LINK} git && mv git/* . && rm -rf git", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-print("Starting process with command `python3 -m thetgbot`")
-print("State changed from starting to up")
-subprocess.run("python3 -m thetgbot", shell=True)
+Repo.clone_from(GITHUB_REPO_LINK, ".ota", branch=GITHUB_REPO_BRANCH)
+copytree(".ota", ".", dirs_exist_ok=True)
+rmtree(".ota")
+import thetgbot
